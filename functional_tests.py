025cb447da806b11619b96dd143af0eefe933432
Lucas Castro <lucasengecomp@gmail.com>
2021-05-14 20:42:05 -0300

    First unittest test case

--- functional_tests.py
+++ functional_tests.py
@@ -32,39 +32,48 @@ class NewVsitorTest(unittest.TestCase):
         self.browser.quit()
 
 
-# Edith ouviu falar que agora a aplicação online de lista de tarefas
-# aceita definir prioridades nas tarefas do tipo baixa, média e alta
-# Ela decide verificar a homepage
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith ouviu falar que agora a aplicação online de lista de tarefas
+        # aceita definir prioridades nas tarefas do tipo baixa, média e alta
+        # Ela decide verificar a homepage
+        
+        self.browser.get("http://localhost:8000")
 
-# Ela percebe que o título da página e o cabeçalho mencionam
-# listas de tarefas com prioridade (priority to-do)
+        # Ela percebe que o título da página e o cabeçalho mencionam
+        # listas de tarefas com prioridade (priority to-do)
+        
+        self.assertIn('priority to-Do', self.browser.title)
+        self.fail('Finish the test!')
 
-# Ela é convidada a inserir um item de tarefa e a prioridade da 
-# mesma imediatamente
-
-
-# Ela digita "Comprar anzol" em uma nova caixa de texto
-# e assinala prioridade alta no campo de seleção de prioridades
-
-# Quando ela tecla enter, a página é atualizada, e agora
-# a página lista "1 - Comprar anzol - prioridade alta"
-# como um item em uma lista de tarefas
-
-# Ainda continua havendo uma caixa de texto convidando-a a 
-# acrescentar outro item. Ela insere "Comprar cola instantâne"
-# e assinala prioridade baixa pois ela ainda tem cola suficiente
-# por algum tempo
-
-# A página é atualizada novamente e agora mostra os dois
-# itens em sua lista e as respectivas prioridades
-
-# Edith se pergunta se o site lembrará de sua lista. Então
-# ela nota que o site gerou um URL único para ela -- há um
-# pequeno texto explicativo para isso.
-
-# Ela acessa essa URL -- sua lista de tarefas continua lá.
+        # Ela é convidada a inserir um item de tarefa e a prioridade da 
+        # mesma imediatamente
+        
+        
+        # Ela digita "Comprar anzol" em uma nova caixa de texto
+        # e assinala prioridade alta no campo de seleção de prioridades
+        
+        # Quando ela tecla enter, a página é atualizada, e agora
+        # a página lista "1 - Comprar anzol - prioridade alta"
+        # como um item em uma lista de tarefas
+        
+        # Ainda continua havendo uma caixa de texto convidando-a a 
+        # acrescentar outro item. Ela insere "Comprar cola instantâne"
+        # e assinala prioridade baixa pois ela ainda tem cola suficiente
+        # por algum tempo
+        
+        # A página é atualizada novamente e agora mostra os dois
+        # itens em sua lista e as respectivas prioridades
+        
+        # Edith se pergunta se o site lembrará de sua lista. Então
+        # ela nota que o site gerou um URL único para ela -- há um
+        # pequeno texto explicativo para isso.
+        
+        # Ela acessa essa URL -- sua lista de tarefas continua lá.
 
  
 
 ################################# FIM ####################################
 
+
+if __name__ == '__main__':
+        unittest.main()
