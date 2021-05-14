e3a301dbaef687124c1771940a48161e68983cd8
Lucas Castro <lucasengecomp@gmail.com>
2021-05-14 20:53:59 -0300

    First unit test and url mapping, dummy view

--- superlists_ESBD_2/urls.py
+++ superlists_ESBD_2/urls.py
@@ -1,4 +1,4 @@
-"""superlists_ESBD_2 URL Configuration
+"""superlists URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
+from lists import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home_page, name='home'),
 ]
